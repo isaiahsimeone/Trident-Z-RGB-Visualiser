85e7d4e4a3f1032dfa6ac7252b642d3b59439d29
isaiah <isaiahsimeone@gmail.com>
2019-07-20 22:10:32 +1000

    Code cleanup

--- src/sim/SlotController.java
+++ src/sim/SlotController.java
@@ -3,7 +3,6 @@ package sim;
 import io.dvlopt.linux.i2c.*;
 import java.io.IOException;
 
-
 public class SlotController implements sim.constant.Modes, sim.constant.Registers {
     /** The bus used to control the slot **/
     private I2CBus bus = null;
@@ -29,14 +28,17 @@ public class SlotController implements sim.constant.Modes, sim.constant.Register
 
     }
 
+    /**
+     * Maps RAM modules on 119
+     * @throws IOException
+     */
     public void mapModule() throws IOException {
-        bus.selectSlave(119);           // 0x77
+        bus.selectSlave(119);
         try {
-
             writeRegister(REG_SLOT_INDEX, slotNumber);
-            writeRegister(REG_I2C_ADDRESS, 0xE0 + (slotNumber << 1));   // slot << 1 == slot*2
+            writeRegister(REG_I2C_ADDRESS, 224 + (slotNumber*2));
         } catch (IOException e) {
-            // Module has already been mapped
+            // RAM Module has already been mapped
         }
     }
 
@@ -50,8 +52,6 @@ public class SlotController implements sim.constant.Modes, sim.constant.Register
         this.setDirect(0);
         writeRegister(REG_MODE, mode);
         writeRegister(REG_APPLY, APPLY_VAL);
-
-        System.out.println("Set Mode ("+slotNumber+"): "+mode);
     }
 
     /**
@@ -74,11 +74,22 @@ public class SlotController implements sim.constant.Modes, sim.constant.Register
         return name;
     }
 
+    /**
+     * Returns the slave address which this class will use to control the delegated RAM module.
+     * @return Slave address associated with this class.
+     */
     public int getSlaveAddress() {
         return slaveAddress;
     }
 
-
+    /**
+     *
+     * @param LED Which LED to set the colour of (e.g. 0 denotes the right-most LED, 4 denotes the left-most.)
+     * @param red The red component of the colour which the LED will display.
+     * @param green The green component of the colour which the LED will display.
+     * @param blue The blue component of the colour which the LED will display.
+     * @throws IOException
+     */
     public void setDirectColour(int LED, int red, int green, int blue) throws IOException {
         this.setDirect(1);
         writeWord(0x00, (((REG_COLORS_DIRECT + (3*LED)) << 8) & 0xFF00) | (((REG_COLORS_DIRECT + (3*LED)) >> 8) & 0x00FF));
@@ -101,7 +112,13 @@ public class SlotController implements sim.constant.Modes, sim.constant.Register
     }
 
 
-
+    /**
+     * Generates a Block with colouring information used to set LED colours.
+     * @param red The red component of the colour which the LED will display.
+     * @param green The green component of the colour which the LED will display.
+     * @param blue The blue component of the colour which the LED will display.
+     * @return Colour information I2CBlock.
+     */
     public static SMBus.Block getRGBBlock(int red, int green, int blue) {
         SMBus.Block rgbBlock = new SMBus.Block();
         rgbBlock.set(1, red)
--- src/sim/TridentMain.java
+++ src/sim/TridentMain.java
@@ -2,46 +2,31 @@ package sim;
 
 import io.dvlopt.linux.i2c.I2CBus;
 import io.dvlopt.linux.i2c.SMBus;
-import sim.constant.Modes;
 
 import java.io.IOException;
 
 public class TridentMain {
 
     public static void main(String[] args) throws IOException {
-        // Detect all slots here
-        // add them to an array of busses.
-        int[] slaveAddresses = new int[4];
 
-        I2CBus bus0 = new I2CBus("/dev/i2c-0");
-        I2CBus bus1 = new I2CBus("/dev/i2c-0");
-        I2CBus bus2 = new I2CBus("/dev/i2c-0");
-        I2CBus bus3 = new I2CBus("/dev/i2c-0");
+        int[] slaveAddresses = {112, 113, 114, 115};
 
-
+        I2CBus[] busses = new I2CBus[4];
         SlotController[] dimms = new SlotController[4];
 
-        dimms[0] = new SlotController(bus0, 112, 0);
-        dimms[1] = new SlotController(bus1, 113, 1);
-        dimms[2] = new SlotController(bus2, 114, 2);
-        dimms[3] = new SlotController(bus3, 115, 3);
+        for (int i = 0; i < 4; i++) {
+            busses[i] = new I2CBus("/dev/i2c-0");
+            dimms[i] = new SlotController(busses[i], slaveAddresses[i], i);
+        }
 
 
-        //set slot colour mode
         for(int i=0;i<4;i++) {
-            //System.out.println(dimms[1].getName());
-            dimms[i].setEffectMode(0);
-            System.out.println(dimms[i].getSlaveAddress());
-            dimms[i].setDirect(0);
-            //dimms[i].setColour(4, 255, 20, 100);
 
-            //set off?
-            dimms[i].setDirectColour(2, 0, 255, 0);
-            //dimms[i].setDirectColour(1, 0, 0, 0);
-            //dimms[i].setDirectColour(2, 255, 0, 0);
-            //dimms[i].setDirectColour(3, 255, 0, 0);
-            //dimms[i].setDirectColour(4, 0, 0, 0);
-            //dimms[i].setDirectColour(5, 0, 0, 0);
+            dimms[i].setEffectMode(4);
+            System.out.println(dimms[i].getSlaveAddress());
+
+            //dimms[i].setDirectColour(4, 35, 63, 53);
+
         }
     }
 
